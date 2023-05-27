0ade14ca3218d8adfad811f149f80f1b56213516
Godfrey Best <godfrey.c.best@gmail.com>
2023-05-27 14:24:10 -0400

    Add contact page

---
+++ src/app/contact/page.js
@@ -0,0 +1,8 @@
+import Layout from '../../components/Layout';
+import Contact from '../../components/Contact';
+
+export default () => (
+    <Layout contentHeader="Contact Us">
+        <Contact/>
+    </Layout>
+);
