c9bfceb2c6ff51f99655304e34ed91745d88af5a
Godfrey Best <godfrey.c.best@gmail.com>
2023-05-27 13:55:03 -0400

    Add services page

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -2,57 +2,18 @@ import Image from 'next/image';
 import Layout from '../../components/Layout';
 
 export default () => (
-  <Layout contentHeader="About">
-    <h3>
-      Practical Psychology Consultancy Ltd works to support all aspects of your work to ensure your
-      desired outcome.
-    </h3>
-    <p className="about-details">
-      <br />
-      We are a team of experienced and regulated psychologists. Using an evidenced-based approach,
-      we will work with you to develop a bespoke solution to address those safety, health and
-      overall well-being issues, inclusive of mental health, which have arisen in your workplace. We
-      will ensure that the solution adds value to your organisational practices and enhances your
-      organisational culture.
-    </p>
-    <br />
-    <p className="about-details">
-      Dr. Roxane Gervais is a highly goal-oriented professional with strong data analytic and
-      interpretation experience. Highly experienced in research design, survey design, psychological
-      assessment, organisational change, organisational development, psychometrics, and career
-      development. Extremely organised with the ability to manage multiple projects and meet
-      deadlines. Years of experience working in diverse teams and in international environments. A
-      strong work ethic combined with a commitment to excellence in all projects undertaken. Strong
-      communication and presentation skills.
-    </p>
-    <br />
-    <div>
-      <Image
-        className="about-me-pic"
-        fill
-        src="/assets/images/roxane-linkedin.jpeg"
-        alt="linkedIn"
-      />
-    </div>
-    <div className="youtube-area">
-      <div className="youtube-video">
-        <iframe
-          src="https://www.youtube.com/embed/o9Y3M0M_axA"
-          frameBorder="0"
-          title="roxane"
-          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
-      </div>
-      <div className="youtube-video">
-        <iframe
-          src="https://www.youtube.com/embed/aYImNDUTeWU"
-          frameBorder="0"
-          title="employee-wellness"
-          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
-      </div>
-    </div>
-  </Layout>
+    <Layout contentHeader="Services">
+        <div className="services">
+            <h2>Business Support</h2>
+            <p>We are a team of experienced and regulated psychologists. Using an evidenced-based approach, we will work with you to develop a bespoke solution to address those safety, health and overall well-being issues, inclusive of mental health, which have arisen in your workplace. We will ensure that the solution adds value to your organisational practices and enhances your organisational culture.</p>
+            <h2>Research and Survey Design</h2>
+            <p>Our experience and expertise in these areas allow us to design research to meet your needs for whichever issue you wish to gain more insight. We will use research methods and techniques that will obtain the information that you wish to obtain, inclusive of developing accurate and meaningful surveys. Our expertise covers quantitative and qualitative methods and thereby allows us to use a triangulation approach in the research process to ensure a more robust practice.</p>
+            <h2>Addressing Work-Related Stress</h2>
+            <p>The sickness absence levels within organisations remain high in the United Kingdom due to mental ill health conditions, such as work-related stress, anxiety or depression. This workshop will outline an organisation’s legal responsibility in managing work-related stress, which in turn can lead to work-relevant depression and anxiety. It will provide participants with evidence-based solutions that cover the triad structure within any organisation. These are the organisational level, the group or team level and the individual level. We will work with you to understand the causes of the work-related stress in your organisation, as well as knowing what works in supporting workers’ well-being in the workplace and the tools to prevent, and if needed, address the mental ill health conditions that can arise within the workplace.</p>
+            <h2>Working through the Menopause</h2>
+            <p>The awareness about the menopause has increased and this benefits those who are experiencing or will experience this life change as they age. It is even more relevant for those women who are working during this change in their lives, as the workplace can be challenging at times, and the necessary support systems may not be in place. We can provide a one-day evidence-based workshop for those going through the change with the skills needed to function more effectively at home and at work, while ensuring they focus on maintaining their well-being. It allows a supportive environment to address those workplace challenges, at the same time as providing a networking opportunity to those attending.</p>
+            <h2>Leadership Development</h2>
+            <p>Leaders are the essence of an organisation. They provide strategic direction, make essential decisions to ensure that the business&apos;s functionality is effective; they solve any problems that may emerge; and they influence and support the workforce. This bespoke leadership development programme, inclusive of management training will assist the leaders in your organisation to enhance their individuals and business potential as well as improve their ability to gain the trust, respect, commitment and support of their teams.</p>
+        </div>
+    </Layout>
 );
