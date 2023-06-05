30074974d5beb7e3865b05910852632fd8c63c7a
Godfrey Best <godfrey.c.best@gmail.com>
2023-06-05 09:25:35 -0400

    Use next image for slim hero

--- src/components/SlimHero.js
+++ src/components/SlimHero.js
@@ -1,9 +1,9 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default () => (
   <div className="hero-img-wrap">
-    {/* eslint-disable-next-line @next/next/no-img-element */}
-    <img className="hero-background-img" src="/assets/images/work-meeting.jpg" alt="work-meeting" />
+    <Image className="hero-background-img" alt="work-meeting" src="/assets/images/work-meeting.jpg" fill />
     <div className="hero-overlay"></div>
     <div className="hero-content-small">
       <div className="hero-text-small">
